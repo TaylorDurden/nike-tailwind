8291144510a3f0a5857ba21bdd56ffee3afc2d59
taylor <258266864@qq.com>
2024-05-06 16:20:09 +0800

    feature:popular products section

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Button from './Button';
 import Nav from './Nav';
 import ShoeCard from './ShoeCard';
+import ProductCard from './ProductCard';
 
-export { Button, Nav, ShoeCard };
+export { Button, Nav, ShoeCard, ProductCard };
